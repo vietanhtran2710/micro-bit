d7355c9c0277faefc6bede2f194787f475b7432c
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-06-17 21:55:18 +0700

    remove button A press

--- accelerometer/print_data.py
+++ accelerometer/print_data.py
@@ -1,8 +1,7 @@
 from microbit import *
 
 while True:
-    if button_a.is_pressed():
-        x = accelerometer.get_x()
-        y = accelerometer.get_y()
-        z = accelerometer.get_z()
-        print(x, y, z)
+    x = accelerometer.get_x()
+    y = accelerometer.get_y()
+    z = accelerometer.get_z()
+    print(x, y, z)
